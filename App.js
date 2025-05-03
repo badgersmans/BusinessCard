bc418cc0c26fe6e7391072cb9cee606f90532126
Shawn Law <shawnlaw6669@gmail.com>
2025-05-03 19:40:03 +0800

    8_custom components

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView, Button, Linking } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import ProjectCard from './ProjectCard';
 
 function DisplayInsets() {
   const insets = useSafeAreaInsets()
@@ -41,43 +42,53 @@ export default function App() {
       <SafeAreaView edges={['bottom']}>
 
         <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.container}>
+          <View style={styles.container}>
 
-          <Image 
-            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg' }}
-            style={{
-              width: '100%',
-              aspectRatio: 16/9,
-            }}
-          />
+            <Image
+              source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg' }}
+              style={{
+                width: '100%',
+                aspectRatio: 16 / 9,
+              }}
+            />
 
-          <Image 
-            source={require('./assets/vadim.png')} 
-            style={{ 
-              width: 150, 
-              height: 150, 
-              borderRadius: 150, 
-              borderWidth: 5, 
-              borderColor: 'white',
-              marginTop: -75
-            }} 
-          />
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Shawn Law</Text>
-          <Text>fgfgf</Text>
+            <Image
+              source={require('./assets/vadim.png')}
+              style={{
+                width: 150,
+                height: 150,
+                borderRadius: 150,
+                borderWidth: 5,
+                borderColor: 'white',
+                marginTop: -75
+              }}
+            />
+            <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Shawn Law</Text>
+            <Text>fgfgf</Text>
 
-          <View style={{flexDirection: 'row', marginVertical: 10, gap: 10}}>
-            {links.github && <FontAwesome6 name="github" size={24} color="black" />}
-            {links.x && <FontAwesome6 name="x-twitter" size={24} color="black" />}
-            {links.email && <FontAwesome6 name="at" size={24} color="black" />}
+            <View style={{ flexDirection: 'row', marginVertical: 10, gap: 10 }}>
+              {links.github && <FontAwesome6 name="github" size={24} color="black" />}
+              {links.x && <FontAwesome6 name="x-twitter" size={24} color="black" />}
+              {links.email && <FontAwesome6 name="at" size={24} color="black" />}
+            </View>
+
+            <Button title='Contact Me' onPress={onContactMe} />
+
+            <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque, temporibus porro odio at ipsam praesentium voluptatum rem officia error! Fugit, aperiam cum necessitatibus soluta sint earum sed, magni
+            </Text>
+
+            <Text style={{ fontWeight: 'bold', fontSize: 20, marginTop: 20 }}>Projects</Text>
+            
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{gap: 15, padding: 10}}>
+              <ProjectCard name="Apple Cards" image={require('./assets/projects/project1.jpeg')}/>
+              <ProjectCard name="Trello" image={require('./assets/projects/project2.jpeg')}/>
+              <ProjectCard name="Flappy Bird" image={require('./assets/projects/project3.jpeg')}/>
+              <ProjectCard name="Todo app" image={require('./assets/projects/project4.jpeg')}/>
+            </ScrollView>
+
+            <StatusBar style="auto" />
           </View>
-
-          <Button title='Contact Me' onPress={onContactMe}/>
-
-          <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque, temporibus porro odio at ipsam praesentium voluptatum rem officia error! Fugit, aperiam cum necessitatibus soluta sint earum sed, magni, eaque voluptatibus eos modi esse repellendus! Tempore, eligendi consectetur enim pariatur illum vero, vel dolores maxime debitis libero reiciendis at laudantium. Officia dolorem animi explicabo quos amet odio nulla? Tempore, ratione quae voluptatem possimus placeat facere sequi sed reprehenderit. Ullam laboriosam quae quibusdam esse unde incidunt, tempore deserunt voluptatibus, aliquam minima quisquam sed fugit fuga nobis debitis libero similique rerum minus numquam aspernatur itaque doloribus soluta dolore? Enim cupiditate minima aut totam molestiae magni optio odit. Culpa doloribus quibusdam pariatur iusto laboriosam sapiente! Error minima tempore voluptas debitis voluptates placeat voluptate perferendis odio esse repudiandae eum quasi soluta laboriosam, ad, magnam culpa, consequuntur vitae excepturi quibusdam. Eum nesciunt veniam sequi reiciendis, laboriosam exercitationem quos magni voluptatibus. Enim debitis, nobis repellat eum optio consequuntur ipsum adipisci eaque beatae incidunt doloribus nisi iste obcaecati blanditiis labore dolor aspernatur excepturi unde aliquid accusantium perferendis, aliquam odio cumque ratione. Ullam praesentium velit illum eligendi autem magnam eos iusto inventore repellat commodi, molestias placeat a quos recusandae animi error iste rem perspiciatis laboriosam fugit delectus neque?
-          </Text> 
-          <StatusBar style="auto" />
-        </View>
         </ScrollView>
       </SafeAreaView>
     </SafeAreaProvider>
---
+++ ProjectCard.jsx
@@ -0,0 +1,20 @@
+import {Text, View, Image } from 'react-native';
+
+export default function ProjectCard({name, image}) {
+  return (
+    <View>
+      <Image
+        source={image}
+        style={{ height: 150, aspectRatio: 16 / 9, borderRadius: 10 }}
+      />
+      <Text style={{
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: 'dimgray',
+        marginTop: 10
+      }}>
+        {name}
+      </Text>
+    </View>
+  )
+}
