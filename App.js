779daf726e45f057f974e0eb520f5c60c252a366
Shawn Law <shawnlaw6669@gmail.com>
2025-05-03 13:48:29 +0800

    5_buttons

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, Button, Linking } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 export default function App() {
+
+  const onContactMe = () => {
+    Linking.openURL('mailto:shawnlaw666@proton.me')
+  }
+
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
     <View style={styles.container}>
@@ -34,6 +40,8 @@ export default function App() {
         <FontAwesome6 name="x-twitter" size={24} color="black" />
         <FontAwesome6 name="at" size={24} color="black" />
       </View>
+
+      <Button title='Contact Me' onPress={onContactMe}/>
 
       <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque, temporibus porro odio at ipsam praesentium voluptatum rem officia error! Fugit, aperiam cum necessitatibus soluta sint earum sed, magni, eaque voluptatibus eos modi esse repellendus! Tempore, eligendi consectetur enim pariatur illum vero, vel dolores maxime debitis libero reiciendis at laudantium. Officia dolorem animi explicabo quos amet odio nulla? Tempore, ratione quae voluptatem possimus placeat facere sequi sed reprehenderit. Ullam laboriosam quae quibusdam esse unde incidunt, tempore deserunt voluptatibus, aliquam minima quisquam sed fugit fuga nobis debitis libero similique rerum minus numquam aspernatur itaque doloribus soluta dolore? Enim cupiditate minima aut totam molestiae magni optio odit. Culpa doloribus quibusdam pariatur iusto laboriosam sapiente! Error minima tempore voluptas debitis voluptates placeat voluptate perferendis odio esse repudiandae eum quasi soluta laboriosam, ad, magnam culpa, consequuntur vitae excepturi quibusdam. Eum nesciunt veniam sequi reiciendis, laboriosam exercitationem quos magni voluptatibus. Enim debitis, nobis repellat eum optio consequuntur ipsum adipisci eaque beatae incidunt doloribus nisi iste obcaecati blanditiis labore dolor aspernatur excepturi unde aliquid accusantium perferendis, aliquam odio cumque ratione. Ullam praesentium velit illum eligendi autem magnam eos iusto inventore repellat commodi, molestias placeat a quos recusandae animi error iste rem perspiciatis laboriosam fugit delectus neque?
