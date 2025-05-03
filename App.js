7e61681def82e87f458ccf26919be61957295298
Shawn Law <shawnlaw6669@gmail.com>
2025-05-03 14:59:00 +0800

    6_safeareaview

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView, Button, Linking } from 'react-native';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function DisplayInsets() {
+  const insets = useSafeAreaInsets()
+  return (
+    <Text>Insets: Top {insets.top} - Bottom {insets.bottom} </Text>
+  )
+}
+
 
 export default function App() {
 
@@ -8,48 +17,66 @@ export default function App() {
     Linking.openURL('mailto:shawnlaw666@proton.me')
   }
 
+  // return (
+  //   <SafeAreaProvider>
+
+  //   <SafeAreaView edges={['top']} style={{ backgroundColor: 'red' }}>
+  //       <View style={{ backgroundColor: 'palegreen', height: '100%', alignItems: 'center' }}>
+  //         <Text>Title</Text>
+  //         <DisplayInsets />
+  //         <Text style={{ marginTop: 'auto' }}>Footer</Text>
+  //       </View>
+  //   </SafeAreaView >
+  //   </SafeAreaProvider>
+  // )
+
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-    <View style={styles.container}>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
 
-      <Image 
-        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg' }}
-        style={{
-          width: '100%',
-          aspectRatio: 16/9,
-        }}
-      />
+        <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
 
-      <Image 
-        source={require('./assets/vadim.png')} 
-        style={{ 
-          width: 150, 
-          height: 150, 
-          borderRadius: 150, 
-          borderWidth: 5, 
-          borderColor: 'white',
-          marginTop: -75
-        }} 
-      />
-      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Shawn Law</Text>
-      <Text>fgfgf</Text>
+          <Image 
+            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg' }}
+            style={{
+              width: '100%',
+              aspectRatio: 16/9,
+            }}
+          />
 
-      <View style={{flexDirection: 'row', marginVertical: 10, gap: 10}}>
-        <FontAwesome6 name="github" size={24} color="black" />
-        <FontAwesome6 name="x-twitter" size={24} color="black" />
-        <FontAwesome6 name="at" size={24} color="black" />
-      </View>
+          <Image 
+            source={require('./assets/vadim.png')} 
+            style={{ 
+              width: 150, 
+              height: 150, 
+              borderRadius: 150, 
+              borderWidth: 5, 
+              borderColor: 'white',
+              marginTop: -75
+            }} 
+          />
+          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Shawn Law</Text>
+          <Text>fgfgf</Text>
 
-      <Button title='Contact Me' onPress={onContactMe}/>
+          <View style={{flexDirection: 'row', marginVertical: 10, gap: 10}}>
+            <FontAwesome6 name="github" size={24} color="black" />
+            <FontAwesome6 name="x-twitter" size={24} color="black" />
+            <FontAwesome6 name="at" size={24} color="black" />
+          </View>
 
-      <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque, temporibus porro odio at ipsam praesentium voluptatum rem officia error! Fugit, aperiam cum necessitatibus soluta sint earum sed, magni, eaque voluptatibus eos modi esse repellendus! Tempore, eligendi consectetur enim pariatur illum vero, vel dolores maxime debitis libero reiciendis at laudantium. Officia dolorem animi explicabo quos amet odio nulla? Tempore, ratione quae voluptatem possimus placeat facere sequi sed reprehenderit. Ullam laboriosam quae quibusdam esse unde incidunt, tempore deserunt voluptatibus, aliquam minima quisquam sed fugit fuga nobis debitis libero similique rerum minus numquam aspernatur itaque doloribus soluta dolore? Enim cupiditate minima aut totam molestiae magni optio odit. Culpa doloribus quibusdam pariatur iusto laboriosam sapiente! Error minima tempore voluptas debitis voluptates placeat voluptate perferendis odio esse repudiandae eum quasi soluta laboriosam, ad, magnam culpa, consequuntur vitae excepturi quibusdam. Eum nesciunt veniam sequi reiciendis, laboriosam exercitationem quos magni voluptatibus. Enim debitis, nobis repellat eum optio consequuntur ipsum adipisci eaque beatae incidunt doloribus nisi iste obcaecati blanditiis labore dolor aspernatur excepturi unde aliquid accusantium perferendis, aliquam odio cumque ratione. Ullam praesentium velit illum eligendi autem magnam eos iusto inventore repellat commodi, molestias placeat a quos recusandae animi error iste rem perspiciatis laboriosam fugit delectus neque?
-      </Text>
+          <Button title='Contact Me' onPress={onContactMe}/>
 
-      <StatusBar style="auto" />
-    </View>
-    </ScrollView>
+          <Text style={{ padding: 10, fontSize: 16, lineHeight: 20 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium cumque, temporibus porro odio at ipsam praesentium voluptatum rem officia error! Fugit, aperiam cum necessitatibus soluta sint earum sed, magni, eaque voluptatibus eos modi esse repellendus! Tempore, eligendi consectetur enim pariatur illum vero, vel dolores maxime debitis libero reiciendis at laudantium. Officia dolorem animi explicabo quos amet odio nulla? Tempore, ratione quae voluptatem possimus placeat facere sequi sed reprehenderit. Ullam laboriosam quae quibusdam esse unde incidunt, tempore deserunt voluptatibus, aliquam minima quisquam sed fugit fuga nobis debitis libero similique rerum minus numquam aspernatur itaque doloribus soluta dolore? Enim cupiditate minima aut totam molestiae magni optio odit. Culpa doloribus quibusdam pariatur iusto laboriosam sapiente! Error minima tempore voluptas debitis voluptates placeat voluptate perferendis odio esse repudiandae eum quasi soluta laboriosam, ad, magnam culpa, consequuntur vitae excepturi quibusdam. Eum nesciunt veniam sequi reiciendis, laboriosam exercitationem quos magni voluptatibus. Enim debitis, nobis repellat eum optio consequuntur ipsum adipisci eaque beatae incidunt doloribus nisi iste obcaecati blanditiis labore dolor aspernatur excepturi unde aliquid accusantium perferendis, aliquam odio cumque ratione. Ullam praesentium velit illum eligendi autem magnam eos iusto inventore repellat commodi, molestias placeat a quos recusandae animi error iste rem perspiciatis laboriosam fugit delectus neque?
+          </Text> 
+          <StatusBar style="auto" />
+        </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
+
   );
 }
 
