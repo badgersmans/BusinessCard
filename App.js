b4ca0fb8ca2e391e0dce0e5d7dddca7470aa1674
Shawn Law <shawnlaw6669@gmail.com>
2025-05-03 13:05:47 +0800

    2_Images

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <Image 
+        source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/pinterest/0.jpeg' }}
+        style={{
+          width: '100%',
+          aspectRatio: 16/9,
+        }}
+      />
+
+      <Image 
+        source={require('./assets/vadim.png')} 
+        style={{ 
+          width: 150, 
+          height: 150, 
+          borderRadius: 150, 
+          borderWidth: 5, 
+          borderColor: 'white',
+          marginTop: -75
+        }} 
+      />
+      <Text style={{ fontSize: 30, fontWeight: 'bold' }}>Shawn Law</Text>
       <Text>fgfgf</Text>
       <StatusBar style="auto" />
     </View>
